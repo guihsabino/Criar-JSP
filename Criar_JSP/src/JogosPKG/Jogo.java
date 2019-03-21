6f0dc2b11dcb4387c1f1674fc2f110df15bf81ff
Guilherme Sabino <42854332+guihsabino@users.noreply.github.com>
2019-03-20 23:07:02 -0300

    Add files via upload

---
+++ Criar_JSP/src/JogosPKG/Jogo.java
@@ -0,0 +1,31 @@
+package JogosPKG;
+
+public class Jogo {
+	private String nome;
+	private String genero;
+	private float preco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+}
